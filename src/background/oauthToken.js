713e7ffc51d0551c8590197ebbb98b1e4cd36f0e
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-02-18 07:51:03 +0300

    token on symbols

--- src/background/oauthToken.js
+++ src/background/oauthToken.js
@@ -1,7 +1,18 @@
+const TOKEN_FIELD = Symbol('token');
+const SECRET_FIELD = Symbol('secret');
+
 export default class OAuthToken {
 	constructor(token, secret) {
-		this.token = token;
-		this.secret = secret;
+		this[TOKEN_FIELD] = token;
+		this[SECRET_FIELD] = secret;
+	}
+
+	get token() {
+		return this[TOKEN_FIELD];
+	}
+
+	get secret() {
+		return this[SECRET_FIELD];
 	}
 
 	serialize() {
