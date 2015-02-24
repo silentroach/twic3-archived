3f9810e3ef4b11d59b4b5ed06086e68f57b1dc6d
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-02-24 09:52:37 +0300

    simple model test

---
+++ test/model.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+import Model from '../src/background/model';
+
+describe('Model', function() {
+	it('should be marked as changed after markAsChanged called', function() {
+		var m = new Model();
+
+		assert.equal(m.isChanged(), false);
+
+		m.markAsChanged();
+
+		assert.equal(m.isChanged(), true);
+		assert.property(m, 'updateTime');
+		assert.equal(m.updateTime <= Date.now(), true);
+	});
+});
