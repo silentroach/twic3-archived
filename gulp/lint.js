00f6ba0c4a552aa495eee42adaa2862da00d03f8
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-02-28 12:32:52 +0300

    trying to use babel-eslint, but not so great

--- gulp/lint.js
+++ gulp/lint.js
@@ -2,9 +2,8 @@ var gulp = require('gulp');
 var gulpEslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
-	gulp.src('src/background/*.js')
-		.pipe(gulpEslint({
-			// @todo wait for normal es6 support
-		}))
-		.pipe(gulpEslint.format());
+	return gulp.src('src/background/*.js')
+		.pipe(gulpEslint())
+		.pipe(gulpEslint.format())
+		.pipe(gulpEslint.failAfterError());
 });
