66b2afa53feea98de667aa75e750c4e0498b4ca9
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-04-01 22:50:01 +0300

    todo output

--- gulp/dev.js
+++ gulp/dev.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var gulpTodos = require('gulp-todo');
+var through = require('through2');
 
 gulp.task('todo', function() {
 	return gulp.src([
-			'src/**/*.*(js|jsx)',
-			'gulp/*.js',
-			'test/**/*.js',
-			'!src/vendor/*'
-		])
-		.pipe(gulpTodos())
-		.pipe(gulp.dest('./'));
+		'src/**/*.*(js|jsx)',
+		'gulp/*.js',
+		'test/**/*.js',
+		'!src/vendor/*'
+	])
+	.pipe(gulpTodos({
+		reporter: 'table'
+	}))
+	.pipe(through.obj(function(file, enc, callback) {
+		console.log(file.contents.toString());
+		callback();
+	}));
 });
--- gulp/lint.js
+++ gulp/lint.js
@@ -3,12 +3,12 @@ var gulpEslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
 	return gulp.src([
-			'src/**/*.*(js|jsx)',
-			'gulp/*.js',
-			'test/**/*.js',
-			'!src/vendor/*'
-		])
-		.pipe(gulpEslint())
-		.pipe(gulpEslint.format())
-		.pipe(gulpEslint.failAfterError());
+		'src/**/*.*(js|jsx)',
+		'gulp/*.js',
+		'test/**/*.js',
+		'!src/vendor/*'
+	])
+	.pipe(gulpEslint())
+	.pipe(gulpEslint.format())
+	.pipe(gulpEslint.failAfterError());
 });
