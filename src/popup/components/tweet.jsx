aff04981595188dfa09763282a64c21025b563ba
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-04-21 09:07:18 +0300

    display original retweet data

--- src/popup/components/tweet.jsx
+++ src/popup/components/tweet.jsx
@@ -7,16 +7,14 @@ import Avatar from './avatar';
 export default class Tweet extends React.Component {
 	render() {
 		const tweet = this.props.data;
+		const tweetData = tweet.retweeted ? tweet.retweeted : tweet;
 
 		return (
 			<li className="tweet">
-				<a
-					href={'#users/' + tweet.user.id}
-					title={'@' + tweet.user.screenName}
-				>
-					<Avatar template={tweet.user.avatar} />
+				<a href={'#users/' + tweetData.user.id} title={'@' + tweetData.user.screenName}>
+					<Avatar template={tweetData.user.avatar} />
 				</a>
-				<span className="tweet-text" dangerouslySetInnerHTML={{__html: tweet.text }} />
+				<span className="tweet-text" dangerouslySetInnerHTML={{ __html: tweetData.text }} />
 			</li>
 		);
 	}
