76c91d61b85b262077398ede725e1da68c9e20fb
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-05-19 10:09:20 +0300

    better gallery items size calculation

--- src/popup/components/tweet/gallery.jsx
+++ src/popup/components/tweet/gallery.jsx
@@ -2,17 +2,34 @@ import React from 'react';
 
 import './gallery.styl';
 
+import device from '../../device';
+
+const MAX_WIDTH = 100 * (device.isRetina ? 2 : 1);
+
 export default class Gallery extends React.Component {
 	render() {
-		console.log(this.props.items);
-
 		return (
 			<ul className="gallery">
 				{
-					this.props.items.map(item => {
+					this.props.items.map((item, key) => {
+						let minSize, minSizeAlias;
+
+						for (let alias of Object.keys(item.sizes)) {
+							let sizes = item.sizes[alias];
+
+							if (sizes[0] >= MAX_WIDTH
+								&& (sizes[0] < minSize
+									|| undefined === minSize
+								)
+							) {
+								minSize = sizes[0];
+								minSizeAlias = alias;
+							}
+						}
+
 						return (
-							<a className="gallery-item" target="_blank" href={item.url}>
-								<img src={item.imageUrl + ':thumb'} />
+							<a className="gallery-item" target="_blank" href={item.url} key={key}>
+								<img src={item.imageUrl + (minSizeAlias ? ':' + minSizeAlias : '')} />
 							</a>
 						);
 					})
