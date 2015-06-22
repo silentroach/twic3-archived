f70e9e7da88be972a3ae6dd6da6c388237d70d9e
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-06-22 13:22:58 +0300

    loader component test

--- test/src/ui/loader.jsx
+++ test/src/ui/loader.jsx
@@ -1,11 +1,29 @@
 import React from 'react/addons';
 
-import Map from 'ui/loader';
+import Loader from 'ui/loader';
 
 const TestUtils = React.addons.TestUtils;
 
 describe('UI.Loader', function() {
 
-	it('should render');
+	it('should be hidden by default and visible after some timeout', function(done) {
+		const component = TestUtils.renderIntoDocument(
+			<Loader />
+		);
+
+		assert.ok(component);
+
+		const div = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+		assert.ok(div);
+
+		const node = div.getDOMNode();
+
+		assert(node.classList.contains('loader-hidden'), 'not hidden');
+
+		setTimeout(function() {
+			assert(!node.classList.contains('loader-hidden'), 'not visible after timeout');
+			done();
+		}, 250);
+	});
 
 });
