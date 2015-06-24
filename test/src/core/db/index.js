e5e5d1314bd0423fc08b76a143795219eedff5b8
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-06-24 14:24:50 +0300

    db migrations order test

--- test/src/core/db/index.js
+++ test/src/core/db/index.js
@@ -17,25 +17,32 @@ describe('DB', function() {
 	describe('upgrade', function() {
 		let db;
 		let counter = 0;
+		let order = [];
 
 		beforeEach(function() {
+			order = [];
+
 			// can already exists after remove cause it is async
 			db = new DB('uptest' + (counter++));
 
 			db.registerMigration(1, function(instance) {
+				order.push(1);
 				instance.createObjectStore('temp');
 			});
 
+			db.registerMigration(3, function(instance) {
+				order.push(3);
+				instance.createObjectStore('temp2');
+			});
+
 			db.registerMigration(1, function(instance) {
+				order.push(1);
 				instance.createObjectStore('temp5');
 			});
 
 			db.registerMigration(2, function(instance) {
+				order.push(2);
 				instance.deleteObjectStore('temp');
-			});
-
-			db.registerMigration(3, function(instance) {
-				instance.createObjectStore('temp2');
 			});
 		});
 
@@ -53,6 +60,17 @@ describe('DB', function() {
 
 		it('should reject non-existant store access', function() {
 			return assert.isRejected(db.getStore('temp'));
+		});
+
+		it('should call migrations in order', function() {
+			return assert.becomes(
+				db
+					.getStore('temp5')
+					.then(function() {
+						return order;
+					}),
+				[1, 1, 2, 3]
+			);
 		});
 	});
 });
