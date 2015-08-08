1be34c83fe6e3c53f7e26b0ae45f2e14f41e086e
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-08-08 21:36:15 +0300

    do not process node_modules by babel exluding twic-prefixed packages

--- src/base/gulp/webpack.js
+++ src/base/gulp/webpack.js
@@ -42,7 +42,7 @@ module.exports = function(gulp, config) {
 			loaders: [
 				{
 					test: /\.jsx?$/,
-					exclude: /node_modules/,
+					exclude: /node_modules\/(?!twic\-)/,
 					loader: 'babel-loader',
 					query: babelOptions
 				},
