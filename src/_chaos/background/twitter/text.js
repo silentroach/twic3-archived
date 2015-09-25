004913cd82a42add7c4b37d9a3b6fb996161f308
Igor Kalashnikov <igor.kalashnikov@me.com>
2015-09-25 23:19:01 +0300

    cleanup

--- src/_chaos/background/twitter/text.js
+++ src/_chaos/background/twitter/text.js
@@ -1,23 +1,7 @@
-// import twitterTextRegexps from '../../vendor/twitter-text';
-
-const utils = { };
-
-// utils.extractUrls = function(text) {
-// 	var urls = [];
-
-// 	text.replace(twitterTextRegexps.url, function(match, all, before, url, protocol, domain, path, query) {
-// 		urls.push(url);
-// 	});
-
-// 	return urls;
-// };
-
-utils.processLineBreaks = function(input) {
+export function processLineBreaks(input) {
 	return input
 		.replace(/[\r\n]/g, '\n')
 		.replace(/\n{2,}/g, '\n\n')  // convert 3+ breaks to 2
 		.trim()
 		.replace(/\n/g, '<br />');
 };
-
-export default utils;
